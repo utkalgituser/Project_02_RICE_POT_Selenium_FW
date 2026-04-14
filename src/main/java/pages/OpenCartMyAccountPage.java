7974b12b9001cb6cffe5acf8654d116c5c93c8d7
Utkal Barik <utkal.barik@gmail.com>
2026-04-14 13:04:13 +0530

    Add Base64 decode, log masking and Javadoc to page classes

--- src/main/java/pages/OpenCartMyAccountPage.java
+++ src/main/java/pages/OpenCartMyAccountPage.java
@@ -4,6 +4,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import utils.ElementUtil;
 
+/**
+ * Page object class representing the OpenCart My Account Page.
+ * Provides methods to interact with elements on the My Account dashboard.
+ */
 public class OpenCartMyAccountPage {
 
     private ElementUtil eleUtil;
@@ -12,16 +16,29 @@ public class OpenCartMyAccountPage {
     private By myOrdersHeading = By.xpath("//h2[text()='My Orders']");
     private By viewOrderHistoryLink = By.xpath("//a[text()='View your order history']");
 
-    // 2. Page constructor
+    /**
+     * Constructor for OpenCartMyAccountPage.
+     * 
+     * @param driver the WebDriver instance
+     */
     public OpenCartMyAccountPage(WebDriver driver) {
         this.eleUtil = new ElementUtil(driver);
     }
 
-    // 3. Page actions
+    /**
+     * Checks if the 'My Orders' heading is displayed on the page.
+     * 
+     * @return true if the heading is visible, false otherwise
+     */
     public boolean isMyOrdersHeadingDisplayed() {
         return eleUtil.isElementDisplayed(myOrdersHeading);
     }
 
+    /**
+     * Checks if the 'View your order history' link is displayed.
+     * 
+     * @return true if the link is visible, false otherwise
+     */
     public boolean isViewOrderHistoryLinkDisplayed() {
         return eleUtil.isElementDisplayed(viewOrderHistoryLink);
     }
